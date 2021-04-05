8ed045fb861fccd508da681da15fc35deaa4ab53
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-04-05 15:40:11 -0400

    cambios

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -157,7 +157,7 @@ License: You must have a valid license purchased only from themeforest(the above
 
 								
 							</ul>
-							<!--end::Menu Nav-->
+							<!--end::Menu Nav -->
 						</div>
 						<!--end::Menu Container-->
 					</div>
